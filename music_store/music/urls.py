5a64ce592d2a1f08d6234d97bd52608a6a003b49
Solomoney <oluwajidesolomon123@gmail.com>
2024-11-10 17:32:16 +0000

    my music_store assignment code

--- music_store/music/urls.py
+++ music_store/music/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.urls import path, include
+
 from . import views
 
+app_name = "music"
 
-urlpatterns = [
-    path("favorite/", views.favorite, name="favorite"),
+urlpatterns =[
+    path("home/", views.home, name="home"),
+    path("album_listing/", views.album_listing, name="album_listing"),
+    path("artist_listing/", views.artist_listing, name="artist_listing"),
 ]
--- music_store/music/views.py
+++ music_store/music/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .models import Album, Artist
+
 # Create your views here.
 
-def favorite(request):
-    return render(request, "music/favorite.html")
+def home(request):
+    return render (request, "pages/home.html")
+
+def album_listing(request):
+    all_albums = Album.objects.order_by("release_date")
+    context = {"albums": all_albums}
+    return render (request, "music/albums.html", context)
+
+def artist_listing(request):
+    all_artists = Artist.objects.order_by("debut_year")
+    context = {"albums": all_artists}
+    return render (request, "music/artists.html", context)
